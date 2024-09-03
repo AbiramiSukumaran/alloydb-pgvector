627df750aefa3437905e00213327e312e839dc56
Abirami Sukumaran <abidsukumaran@gmail.com>
2024-09-03 21:35:51 +0530

    Update HelloWorld.java

--- src/main/java/cloudcode/helloworld/HelloWorld.java
+++ src/main/java/cloudcode/helloworld/HelloWorld.java
@@ -1,4 +1,24 @@
-package gcfv2;
+
+/* Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
+package gcfv2; 
+/* Change the above line to: 
+package cloudcode.helloworld;
+if you are creating the Google Cloud Function from the Cloud Shell Editor instead of the Cloud Run Functions Console.
+*/
 
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
@@ -19,6 +39,12 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 
 public class HelloHttpFunction implements HttpFunction {
+
+/* Change the above line to:
+public class HelloWorld implements HttpFunction {
+if you are creating the Google Cloud Function from the Cloud Shell Editor instead of the Cloud Run Functions Console.
+*/
+ 
  @Override
   public void service(HttpRequest request, HttpResponse response) throws Exception {
 
