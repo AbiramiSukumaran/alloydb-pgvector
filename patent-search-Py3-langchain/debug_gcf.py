90b7f7ecd4a3c1bc8e5b4a579d1136201796109d
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2024-09-25 20:24:42 +0530

    Update patent-search-Py3-langchain/debug_gcf.py

--- patent-search-Py3-langchain/debug_gcf.py
+++ patent-search-Py3-langchain/debug_gcf.py
@@ -12,22 +12,6 @@ def entrypoint():
     return hello_http(request)
 
 
-class GCFRequest:
-    def __init__(self, data):
-        self.data = data
-
-    def get_json(self):
-        return json.loads(json.dumps(self.data))
-
-
-# Example usage
-trialRunObject = GCFRequest(
-    {
-        "search": "A new Natural Language Processing related Machine Learning Model",
-        "log_level": "NO_DEBUG",
-    }
-)
-
 print("Route / ==> Load data via Langchain")
 
 if __name__ == "__main__":
